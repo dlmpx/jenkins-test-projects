28062b2449ced3acd6eadae147aba9072f707598
dlmpx <dlmpx920@163.com>
2024-01-31 15:57:20 +0800

    first commit,repo init

---
+++ src/main/java/cn/dlmpx/ApplicationStartUp.java
@@ -0,0 +1,12 @@
+package cn.dlmpx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStartUp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStartUp.class,args);
+    }
+}
+
---
+++ src/main/java/cn/dlmpx/controller/HelloController.java
@@ -0,0 +1,21 @@
+package cn.dlmpx.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @projectName: jinkens-test
+ * @package: cn.dlmpx.controller
+ * @className: HelloController
+ * @author: dlmpx
+ * @description: TODO
+ * @date: 2024/1/26 15:12
+ * @version: 1.0
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Jenkins!";
+    }
+}
